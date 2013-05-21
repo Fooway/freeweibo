f41490b02ebda29b23eaa196a7001e92316a17c6
xiaolei <tristones.liu@gmail.com>
2013-05-21 09:54:21 +0800

    fix port bug and tweet save bug

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ worker.stderr.on('data', function(data) {
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
