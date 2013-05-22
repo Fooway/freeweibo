c119737ef154c537ad566348b5597ecce78a586b
xiaolei <xiaolei@li205-226.members.linode.com>
2013-05-21 21:13:21 -0400

    backup script

---
+++ filterback.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+DIR=/var/www/filterback
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+NODE_PATH=/usr/local/lib/node_modules
+NODE=/usr/bin/node
+
+test -x $NODE || exit 0
+
+function start_app {
+  NODE_ENV=production nohup "$NODE" "$DIR/app.js" 1>>"$DIR/logs/filterback.log" 2>&1 &
+  echo $! > "$DIR/pids/filterback.pid"
+}
+
+function stop_app {
+  kill `cat $DIR/pids/filterback.pid`
+}
+
+case $1 in
+   start)
+      start_app ;;
+    stop)
+      stop_app ;;
+    restart)
+      stop_app
+      start_app
+      ;;
+    *)
+      echo "usage: filterback {start|stop}" ;;
+esac
+exit 0
