15d89b31669cef6896b38d75d09f2f1124f2157f
Xiaolei <tristones.liu@gmail.com>
2014-12-01 08:11:36 +0800

    add console log line

--- app/log.js
+++ app/log.js
@@ -2,5 +2,12 @@ var path = require('path');
 var mkdirp= require('mkdirp');
 var log4node = require('log4node');
 var log_dir = path.normalize(__dirname + '/../logs/');
+
 mkdirp.sync(log_dir);
-module.exports =  new log4node.Log4Node({level: 'info', file: log_dir + 'run.log'});
+module.exports =  new log4node.Log4Node({
+  level: 'info',
+  file: log_dir + 'run.log',
+  callback: function (line) {
+    console.log(line);
+  }
+});
